f5cd57d2b521cbb13a6426adc9fd6c05b4c4668a
Naguissa <naguissa@foroelectro.net>
2025-06-26 11:40:37 +0200

    Update to 1.1.0 version - add EEPROM support

--- docs/search/classes_0.js
+++ docs/search/classes_0.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['uespconfiglib_0',['uEspConfigLib',['../classuEspConfigLib.html',1,'']]],
-  ['uespconfiglibfsinterface_1',['uEspConfigLibFSInterface',['../classuEspConfigLibFSInterface.html',1,'']]],
-  ['uespconfiglibfslittlefs_2',['uEspConfigLibFSLittlefs',['../classuEspConfigLibFSLittlefs.html',1,'']]],
-  ['uespconfiglibfsnone_3',['uEspConfigLibFSNone',['../classuEspConfigLibFSNone.html',1,'']]],
-  ['uespconfiglibfssd_4',['uEspConfigLibFSSd',['../classuEspConfigLibFSSd.html',1,'']]],
-  ['uespconfiglibfsspiffs_5',['uEspConfigLibFSSpiffs',['../classuEspConfigLibFSSpiffs.html',1,'']]],
-  ['uespconfigliblist_6',['uEspConfigLibList',['../structuEspConfigLibList.html',1,'']]]
+  ['uespconfiglibfseeprom_1',['uEspConfigLibFSEEPROM',['../classuEspConfigLibFSEEPROM.html',1,'']]],
+  ['uespconfiglibfsinterface_2',['uEspConfigLibFSInterface',['../classuEspConfigLibFSInterface.html',1,'']]],
+  ['uespconfiglibfslittlefs_3',['uEspConfigLibFSLittlefs',['../classuEspConfigLibFSLittlefs.html',1,'']]],
+  ['uespconfiglibfsnone_4',['uEspConfigLibFSNone',['../classuEspConfigLibFSNone.html',1,'']]],
+  ['uespconfiglibfssd_5',['uEspConfigLibFSSd',['../classuEspConfigLibFSSd.html',1,'']]],
+  ['uespconfiglibfsspiffs_6',['uEspConfigLibFSSpiffs',['../classuEspConfigLibFSSpiffs.html',1,'']]],
+  ['uespconfigliblist_7',['uEspConfigLibList',['../structuEspConfigLibList.html',1,'']]]
 ];
--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -2,13 +2,15 @@ var searchData=
 [
   ['uespconfiglib_2ecpp_0',['uEspConfigLib.cpp',['../uEspConfigLib_8cpp.html',1,'']]],
   ['uespconfiglib_2eh_1',['uEspConfigLib.h',['../uEspConfigLib_8h.html',1,'']]],
-  ['uespconfiglibfsinterface_2eh_2',['uEspConfigLibFSInterface.h',['../uEspConfigLibFSInterface_8h.html',1,'']]],
-  ['uespconfiglibfslittlefs_2ecpp_3',['uEspConfigLibFSLittlefs.cpp',['../uEspConfigLibFSLittlefs_8cpp.html',1,'']]],
-  ['uespconfiglibfslittlefs_2eh_4',['uEspConfigLibFSLittlefs.h',['../uEspConfigLibFSLittlefs_8h.html',1,'']]],
-  ['uespconfiglibfsnone_2ecpp_5',['uEspConfigLibFSNone.cpp',['../uEspConfigLibFSNone_8cpp.html',1,'']]],
-  ['uespconfiglibfsnone_2eh_6',['uEspConfigLibFSNone.h',['../uEspConfigLibFSNone_8h.html',1,'']]],
-  ['uespconfiglibfssd_2ecpp_7',['uEspConfigLibFSSd.cpp',['../uEspConfigLibFSSd_8cpp.html',1,'']]],
-  ['uespconfiglibfssd_2eh_8',['uEspConfigLibFSSd.h',['../uEspConfigLibFSSd_8h.html',1,'']]],
-  ['uespconfiglibfsspiffs_2ecpp_9',['uEspConfigLibFSSpiffs.cpp',['../uEspConfigLibFSSpiffs_8cpp.html',1,'']]],
-  ['uespconfiglibfsspiffs_2eh_10',['uEspConfigLibFSSpiffs.h',['../uEspConfigLibFSSpiffs_8h.html',1,'']]]
+  ['uespconfiglibfseeprom_2ecpp_2',['uEspConfigLibFSEEPROM.cpp',['../uEspConfigLibFSEEPROM_8cpp.html',1,'']]],
+  ['uespconfiglibfseeprom_2eh_3',['uEspConfigLibFSEEPROM.h',['../uEspConfigLibFSEEPROM_8h.html',1,'']]],
+  ['uespconfiglibfsinterface_2eh_4',['uEspConfigLibFSInterface.h',['../uEspConfigLibFSInterface_8h.html',1,'']]],
+  ['uespconfiglibfslittlefs_2ecpp_5',['uEspConfigLibFSLittlefs.cpp',['../uEspConfigLibFSLittlefs_8cpp.html',1,'']]],
+  ['uespconfiglibfslittlefs_2eh_6',['uEspConfigLibFSLittlefs.h',['../uEspConfigLibFSLittlefs_8h.html',1,'']]],
+  ['uespconfiglibfsnone_2ecpp_7',['uEspConfigLibFSNone.cpp',['../uEspConfigLibFSNone_8cpp.html',1,'']]],
+  ['uespconfiglibfsnone_2eh_8',['uEspConfigLibFSNone.h',['../uEspConfigLibFSNone_8h.html',1,'']]],
+  ['uespconfiglibfssd_2ecpp_9',['uEspConfigLibFSSd.cpp',['../uEspConfigLibFSSd_8cpp.html',1,'']]],
+  ['uespconfiglibfssd_2eh_10',['uEspConfigLibFSSd.h',['../uEspConfigLibFSSd_8h.html',1,'']]],
+  ['uespconfiglibfsspiffs_2ecpp_11',['uEspConfigLibFSSpiffs.cpp',['../uEspConfigLibFSSpiffs_8cpp.html',1,'']]],
+  ['uespconfiglibfsspiffs_2eh_12',['uEspConfigLibFSSpiffs.h',['../uEspConfigLibFSSpiffs_8h.html',1,'']]]
 ];
