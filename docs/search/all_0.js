5c1297a8d995b4a98d5c6a4b52661042b7ccb0b3
Naguissa <naguissa@foroelectro.net>
2025-12-10 09:01:01 +0100

    Update docs to latest version

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['addoption_0',['addOption',['../classuEspConfigLib.html#a865014747c6c8088c45f0e98b6e26e83',1,'uEspConfigLib']]]
+  ['addoption_0',['addOption',['../classuEspConfigLib.html#abf00358b23bc5489d3e5888521f44033',1,'uEspConfigLib']]]
 ];
--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,9 +1,11 @@
 var searchData=
 [
-  ['uespconfiglibfs_5fstatus_5fclosed_0',['uEspConfigLibFS_STATUS_CLOSED',['../uEspConfigLibFSInterface_8h.html#ac377ca4c668245e8777dfc7435e2b37e',1,'uEspConfigLibFSInterface.h']]],
-  ['uespconfiglibfs_5fstatus_5ferror_1',['uEspConfigLibFS_STATUS_ERROR',['../uEspConfigLibFSInterface_8h.html#a310525379be353bf82ba7c654625f872',1,'uEspConfigLibFSInterface.h']]],
-  ['uespconfiglibfs_5fstatus_5ffatal_2',['uEspConfigLibFS_STATUS_FATAL',['../uEspConfigLibFSInterface_8h.html#ab5f5b6ea490e303183bb937d38106a87',1,'uEspConfigLibFSInterface.h']]],
-  ['uespconfiglibfs_5fstatus_5fnoinit_3',['uEspConfigLibFS_STATUS_NOINIT',['../uEspConfigLibFSInterface_8h.html#a37184b431f476e31456e5cf2bf84826e',1,'uEspConfigLibFSInterface.h']]],
-  ['uespconfiglibfs_5fstatus_5fopen_5fread_4',['uEspConfigLibFS_STATUS_OPEN_READ',['../uEspConfigLibFSInterface_8h.html#a369b8f0a96c56ad2afde9561d70e1aba',1,'uEspConfigLibFSInterface.h']]],
-  ['uespconfiglibfs_5fstatus_5fopen_5fwrite_5',['uEspConfigLibFS_STATUS_OPEN_WRITE',['../uEspConfigLibFSInterface_8h.html#a02bc01bb09cec37788f72361d2ee13c2',1,'uEspConfigLibFSInterface.h']]]
+  ['uespconfiglib_5foption_5fnone_0',['uEspConfigLib_OPTION_NONE',['../uEspConfigLib_8h.html#a2c681494e34658f97d664662cebadc09',1,'uEspConfigLib.h']]],
+  ['uespconfiglib_5foption_5fscanner_1',['uEspConfigLib_OPTION_SCANNER',['../uEspConfigLib_8h.html#a92c73641b28bc7400d20268f776f7676',1,'uEspConfigLib.h']]],
+  ['uespconfiglibfs_5fstatus_5fclosed_2',['uEspConfigLibFS_STATUS_CLOSED',['../uEspConfigLibFSInterface_8h.html#ac377ca4c668245e8777dfc7435e2b37e',1,'uEspConfigLibFSInterface.h']]],
+  ['uespconfiglibfs_5fstatus_5ferror_3',['uEspConfigLibFS_STATUS_ERROR',['../uEspConfigLibFSInterface_8h.html#a310525379be353bf82ba7c654625f872',1,'uEspConfigLibFSInterface.h']]],
+  ['uespconfiglibfs_5fstatus_5ffatal_4',['uEspConfigLibFS_STATUS_FATAL',['../uEspConfigLibFSInterface_8h.html#ab5f5b6ea490e303183bb937d38106a87',1,'uEspConfigLibFSInterface.h']]],
+  ['uespconfiglibfs_5fstatus_5fnoinit_5',['uEspConfigLibFS_STATUS_NOINIT',['../uEspConfigLibFSInterface_8h.html#a37184b431f476e31456e5cf2bf84826e',1,'uEspConfigLibFSInterface.h']]],
+  ['uespconfiglibfs_5fstatus_5fopen_5fread_6',['uEspConfigLibFS_STATUS_OPEN_READ',['../uEspConfigLibFSInterface_8h.html#a369b8f0a96c56ad2afde9561d70e1aba',1,'uEspConfigLibFSInterface.h']]],
+  ['uespconfiglibfs_5fstatus_5fopen_5fwrite_7',['uEspConfigLibFS_STATUS_OPEN_WRITE',['../uEspConfigLibFSInterface_8h.html#a02bc01bb09cec37788f72361d2ee13c2',1,'uEspConfigLibFSInterface.h']]]
 ];
